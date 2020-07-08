48adcac6c1229252f04230913b9c325f6aa7cb7c
ali <ali.nazarudeen14@gmail.com>
2020-07-09 00:05:23 +0530

    Update main.py
    Added text to the window

--- main.py
+++ main.py
@@ -7,6 +7,22 @@ WIDTH, HEIGHT =800, 500
 win = pygame.display.set_mode((WIDTH, HEIGHT))
 pygame.display.set_caption("Hangman Game")
 
+# button variables
+RADIUS = 20
+GAP = 15
+letters = []
+startx = round((WIDTH - (RADIUS * 2 + GAP) * 13) / 2)
+starty = 400
+A = 65
+for i in range(26):
+	x = startx + GAP * 2 + ((RADIUS * 2 + GAP) * (i % 13))
+	y = starty + ((i // 13) * (GAP + RADIUS * 2))
+	letters.append([x, y, chr(A + i)])
+
+
+#fonts
+LETTER_FONT = pygame.font.SysFont('comicsans', 40)
+
 #Images
 
 images = []
@@ -15,32 +31,52 @@ for i in range(7):
 	images.append(image)
 
 
-#game variables
+
+
+#game variable
 hangman_status = 0
 
 #color
 WHITE = (255,255,255)
+BLACK = (0,0,0)
 
 
-#loop
+
 
 
 FPS = 60
 clock = pygame.time.Clock()
 run = True
 
-while run:
-	clock.tick(FPS)									#telling the fps
 
-	win.fill(WHITE)             
-	win.blit(images[hangman_status],(150,100))  	#displays current hangman
-	pygame.display.update()                     	#it is essential to update after every addon..we must refresh manually   
+
+def draw():
+	win.fill(WHITE)  
+
+	#draw buttons
+	for letter in letters:
+		x, y, ltr = letter
+		pygame.draw.circle(win, BLACK, (x, y),RADIUS, 3)
+		text = LETTER_FONT.render(ltr, 1, BLACK)
+		win.blit(text, (x - text.get_width()/2, y - text.get_height()/2))
+
+	
+	win.blit(images[hangman_status],(150,100))
+	pygame.display.update()
+	
+
+#loop to run until close
+while run:
+	clock.tick(FPS)
+	draw()									
+
+	   
 
 	for event in pygame.event.get():
 		if event.type == pygame.QUIT:
-			run = False								#if quit button selected loop closes
+			run = False
 		if event.type == pygame.MOUSEBUTTONDOWN:
-			pos = pygame.mouse.get_pos()			#gets the pointer location when clicked
+			pos = pygame.mouse.get_pos()
 			print(pos)
 
 
